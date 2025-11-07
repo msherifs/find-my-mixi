e6cb261dfeea0f77945e3a9e8867fb1aefb08280
Mohamed Elhawary <mohamedhesham8910@gmail.com>
2025-11-07 19:32:13 +0200

    more map enhancements

--- src/routes/map/index.tsx
+++ src/routes/map/index.tsx
@@ -4,7 +4,7 @@ import {
 	useNavigate,
 } from "@tanstack/react-router";
 import L from "leaflet";
-import { MoreVertical, Navigation } from "lucide-react";
+import { Filter, MoreVertical, Navigation } from "lucide-react";
 import { useEffect, useState } from "react";
 import { renderToString } from "react-dom/server";
 import { useTranslation } from "react-i18next";
@@ -24,6 +24,8 @@ import YellowPin from "@/assets/images/yellow-pin.svg";
 import LocationMarker from "@/components/map/location-marker";
 import { Button } from "@/components/ui/button";
 import "./map.css";
+import MixiSelect from "@/components/shared/mixi-select";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
 
@@ -49,7 +51,7 @@ function RouteComponent() {
 	];
 
 	return (
-		<div className="h-screen w-screen">
+		<div className="h-screen h-[100dvh] w-screen">
 			<MapContainer
 				center={[19.4326, -99.1332]}
 				zoom={50}
@@ -69,29 +71,125 @@ function RouteComponent() {
 							>
 								{isMobile ? "MH" : "Mohamed Hesham"}
 							</Button>
-							{isMobile && (
-								<Button className="w-10 h-10">
-									<MoreVertical />
-								</Button>
-							)}
+							{isMobile && <ActionsDrawer />}
+							{isMobile && <FiltersDrawer />}
 						</div>
 					</header>
 					{!isMobile && (
-						<div className="w-full flex items-center gap-4 border border-[#00000014] rounded-[56px] py-3 px-4 md:px-9 md:h-[78px] bg-white cursor-default"></div>
+						<div className="w-full flex items-center justify-between gap-4 border border-[#00000014] rounded-[56px] py-3 px-9 h-[78px] bg-white cursor-default overflow-x-auto overflow-y-hidden">
+							<div className="flex items-center gap-2 font-[500] text-sm leading-5 tracking-normal text-[#141414]">
+								<Filter className="h-5 w-5" />
+								{t("map.filter")}
+							</div>
+							<div className="flex items-center gap-5">
+								<div className="flex items-center py-2 px-3 gap-3 border border-1 border-[#00000008] rounded-full bg-[#FCFBFB]">
+									<p className="font-medium text-sm leading-5 tracking-normal text-[#0F0F0F]">
+										{t("map.fur")}
+									</p>
+									<div className="flex items-center gap-3">
+										<MixiSelect
+											placeholder={t("map.color")}
+											options={[
+												{ value: "black", label: "Black" },
+												{ value: "white", label: "White" },
+												{ value: "orange", label: "Orange" },
+											]}
+											selectClassName="rounded-full h-[42px]!"
+										/>
+										<MixiSelect
+											placeholder={t("map.pattern")}
+											options={[
+												{ value: "solid", label: "Solid" },
+												{ value: "tabby", label: "Tabby" },
+												{ value: "calico", label: "Calico" },
+											]}
+											selectClassName="rounded-full h-[42px]!"
+										/>
+										<MixiSelect
+											placeholder={t("map.length")}
+											options={[
+												{ value: "short", label: "Short" },
+												{ value: "medium", label: "Medium" },
+												{ value: "long", label: "Long" },
+											]}
+											selectClassName="rounded-full h-[42px]!"
+										/>
+									</div>
+								</div>
+								<div className="flex items-center py-2 px-3 gap-3 border border-1 border-[#00000008] rounded-full bg-[#FCFBFB]">
+									<p className="font-medium text-sm leading-5 tracking-normal text-[#0F0F0F]">
+										{t("map.collar")}
+									</p>
+									<div className="flex items-center gap-3">
+										<MixiSelect
+											placeholder={t("map.color")}
+											options={[
+												{ value: "black", label: "Black" },
+												{ value: "white", label: "White" },
+												{ value: "orange", label: "Orange" },
+											]}
+											selectClassName="rounded-full h-[42px]!"
+										/>
+										<MixiSelect
+											placeholder={t("map.pattern")}
+											options={[
+												{ value: "full", label: "Full" },
+												{ value: "half", label: "Half" },
+												{ value: "bib", label: "Bib" },
+											]}
+											selectClassName="rounded-full h-[42px]!"
+										/>
+										<MixiSelect
+											placeholder={t("map.embellishments")}
+											options={[
+												{ value: "mask", label: "Mask" },
+												{ value: "eye-liner", label: "Eye Liner" },
+												{ value: "boots", label: "Boots" },
+											]}
+											selectClassName="rounded-full h-[42px]!"
+										/>
+									</div>
+								</div>
+								<div className="flex items-center gap-3">
+									<MixiSelect
+										placeholder={t("map.eye_color")}
+										options={[
+											{ value: "brown", label: "Brown" },
+											{ value: "green", label: "Green" },
+											{ value: "orange", label: "Orange" },
+										]}
+										selectClassName="rounded-full h-[42px]!"
+									/>
+									<MixiSelect
+										placeholder={t("map.size")}
+										options={[
+											{ value: "small", label: "Small" },
+											{ value: "medium", label: "Medium" },
+											{ value: "large", label: "Large" },
+										]}
+										selectClassName="rounded-full h-[42px]!"
+									/>
+								</div>
+							</div>
+						</div>
 					)}
 				</div>
 				{!isMobile && (
-					<div className="p-5 rounded-[28px] flex flex-col gap-3 bg-white shadow-[0px_7px_36px_0px_#0000000D] mt-2 w-[90vw] mx-auto md:w-fit md:ml-[5vw] sticky top-0 z-1000">
+					<div className="p-5 rounded-[28px] flex flex-col gap-3 bg-white shadow-[0px_7px_36px_0px_#0000000D] mt-2 w-fit ml-[5vw] sticky top-0 z-1000">
 						<p className="text-gray-700 font-[500] text-[14px] leading-[20px] tracking-[0]">
 							{t("map.quick_actions")}
 						</p>
-						<div className="flex flex-row md:flex-col gap-3 items-start">
-							<Button onClick={() => navigate({ to: "/report-lost-cat" })}>
+						<div className="flex flex-col gap-3 items-start">
+							<Button
+								onClick={() => navigate({ to: "/report-lost-cat" })}
+								className="w-full"
+							>
 								{t("map.report_lost_cat")}
 							</Button>
 							<Button
 								variant={"secondary"}
 								onClick={() => navigate({ to: "/found-lost-cat" })}
+								className="w-full"
 							>
 								{t("map.report_found_cat")}
 							</Button>
@@ -270,4 +368,140 @@ const createCustomIconRed = () => {
 		iconAnchor: [15, 30],
 		popupAnchor: [0, -30],
 	});
+};
+
+const ActionsDrawer = () => {
+	const { t } = useTranslation("");
+	const navigate = useNavigate();
+	return (
+		<Drawer>
+			<DrawerTrigger>
+				<Button className="w-10 h-10">
+					<MoreVertical />
+				</Button>
+			</DrawerTrigger>
+			<DrawerContent className="z-1000 border-none p-5 pt-0! flex flex-col gap-3 bg-white shadow-[0px_7px_36px_0px_#0000000D]">
+				<p className="text-gray-700 font-[500] text-[14px] leading-[20px] tracking-[0]">
+					{t("map.quick_actions")}
+				</p>
+				<div className="flex w-full flex-col gap-3 items-center">
+					<Button
+						onClick={() => navigate({ to: "/report-lost-cat" })}
+						className="w-full"
+					>
+						{t("map.report_lost_cat")}
+					</Button>
+					<Button
+						variant={"secondary"}
+						onClick={() => navigate({ to: "/found-lost-cat" })}
+						className="w-full"
+					>
+						{t("map.report_found_cat")}
+					</Button>
+				</div>
+			</DrawerContent>
+		</Drawer>
+	);
+};
+
+const FiltersDrawer = () => {
+	const { t } = useTranslation("");
+	return (
+		<Drawer>
+			<DrawerTrigger>
+				<Button className="w-10 h-10 text-black" variant={"secondary"}>
+					<Filter />
+				</Button>
+			</DrawerTrigger>
+			<DrawerContent className="z-1000 border-none p-5 pt-0! flex flex-col gap-3 bg-white shadow-[0px_7px_36px_0px_#0000000D]">
+				<p className="text-gray-700 font-[500] text-[14px] leading-[20px] tracking-[0]">
+					{t("map.filter")}
+				</p>
+				<div className="bg-[#FCFBFB] border border-1 border-[#00000008] flex items-start flex-col gap-2 p-4 rounded-2xl w-full">
+					<p className="font-medium text-sm leading-5 tracking-normal text-[#0F0F0F]">
+						{t("map.fur")}
+					</p>
+					<MixiSelect
+						placeholder={t("map.color")}
+						options={[
+							{ value: "black", label: "Black" },
+							{ value: "white", label: "White" },
+							{ value: "orange", label: "Orange" },
+						]}
+						selectClassName="rounded-full"
+					/>
+					<MixiSelect
+						placeholder={t("map.pattern")}
+						options={[
+							{ value: "solid", label: "Solid" },
+							{ value: "tabby", label: "Tabby" },
+							{ value: "calico", label: "Calico" },
+						]}
+						selectClassName="rounded-full"
+					/>
+					<MixiSelect
+						placeholder={t("map.length")}
+						options={[
+							{ value: "short", label: "Short" },
+							{ value: "medium", label: "Medium" },
+							{ value: "long", label: "Long" },
+						]}
+						selectClassName="rounded-full"
+					/>
+				</div>
+				<div className="bg-[#FCFBFB] border border-1 border-[#00000008] flex items-start flex-col gap-2 p-4 rounded-2xl w-full">
+					<p className="font-medium text-sm leading-5 tracking-normal text-[#0F0F0F]">
+						{t("map.collar")}
+					</p>
+					<MixiSelect
+						placeholder={t("map.color")}
+						options={[
+							{ value: "black", label: "Black" },
+							{ value: "white", label: "White" },
+							{ value: "orange", label: "Orange" },
+						]}
+						selectClassName="rounded-full"
+					/>
+					<MixiSelect
+						placeholder={t("map.pattern")}
+						options={[
+							{ value: "full", label: "Full" },
+							{ value: "half", label: "Half" },
+							{ value: "bib", label: "Bib" },
+						]}
+						selectClassName="rounded-full"
+					/>
+					<MixiSelect
+						placeholder={t("map.embellishments")}
+						options={[
+							{ value: "mask", label: "Mask" },
+							{ value: "eye-liner", label: "Eye Liner" },
+							{ value: "boots", label: "Boots" },
+						]}
+						selectClassName="rounded-full"
+					/>
+				</div>
+				<div className="w-full flex items-center gap-2">
+					<MixiSelect
+						placeholder={t("map.eye_color")}
+						options={[
+							{ value: "brown", label: "Brown" },
+							{ value: "green", label: "Green" },
+							{ value: "orange", label: "Orange" },
+						]}
+						selectClassName="rounded-full"
+					/>
+					<MixiSelect
+						placeholder={t("map.size")}
+						options={[
+							{ value: "small", label: "Small" },
+							{ value: "medium", label: "Medium" },
+							{ value: "large", label: "Large" },
+						]}
+						selectClassName="rounded-full"
+					/>
+				</div>
+			</DrawerContent>
+		</Drawer>
+	);
 };
