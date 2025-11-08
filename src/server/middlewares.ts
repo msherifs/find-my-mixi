d4dd8a8deb1404f85d1baa4c8096b8dfb8a85f7b
Mohammed Sherif <hi@msherifs.me>
2025-11-08 21:02:14 +0200

    better stuff-aya

--- src/server/middlewares.ts
+++ src/server/middlewares.ts
@@ -5,8 +5,12 @@ import { redirect } from "@tanstack/react-router";
 // verifies that the user is an admin
 export const adminMiddleware = createMiddleware().server(async ({ next }) => {
   const session = await useAppSession();
+  if (!session.data.role) {
+    throw redirect("/login");
+  }
+
   if (session.data.role !== "ADMIN") {
-    throw redirect("/admin/login");
+    throw redirect("/");
   }
 
   return next();
