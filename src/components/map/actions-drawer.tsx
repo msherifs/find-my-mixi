b815ee6021b317300c84b171e7b1d84713e89d64
Mohamed Elhawary <mohamedhesham8910@gmail.com>
2025-11-08 02:45:26 +0200

    build fix

--- src/components/map/actions-drawer.tsx
+++ src/components/map/actions-drawer.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
-import "./map.css";
 import { useNavigate } from "@tanstack/react-router";
 import { MoreVertical } from "lucide-react";
 import { useTranslation } from "react-i18next";
+import { Button } from "@/components/ui/button";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 
 const ActionsDrawer = () => {
