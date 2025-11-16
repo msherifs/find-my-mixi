2a1b892ca2b6e78184bc6f78785123199cfb0534
Mohamed Elhawary <mohamedhesham8910@gmail.com>
2025-11-16 05:12:25 +0200

    spanish messages

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,10 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslations from "./en.json";
+import esTranslations from "./es.json";
 
 const resources = {
 	en: {
 		translation: enTranslations,
+	},
+	es: {
+		translation: esTranslations,
 	},
 };
 
