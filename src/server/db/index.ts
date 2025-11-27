c6170a5a0f9614f7c4d13fcb271601ed548cfcc9
Mohammed Sherif <hi@msherifs.me>
2025-11-27 20:55:10 +0200

    db name

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -5,8 +5,7 @@ import { Env } from "../utils/env";
 export const papr = new Papr();
 
 export async function initDb() {
-	console.log(Env.DB_URL);
 	const connection = await MongoClient.connect(Env.DB_URL);
-	const db = connection.db("find-my-mixi");
+	const db = connection.db(Env.ENV);
 	papr.initialize(db);
 }
