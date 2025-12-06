2f13f7c111cc8b9f0f8ade543de70a95980bc85a
Mohamed Elhawary <mohamedhesham8910@gmail.com>
2025-12-06 04:26:07 +0200

    minor ui fix

--- src/routes/_admin/admin.index.tsx
+++ src/routes/_admin/admin.index.tsx
@@ -7,10 +7,10 @@ export const Route = createFileRoute("/_admin/admin/")({
 
 function RouteComponent() {
 	return (
-		<div className="flex flex-col items-center justify-center h-full bg-gray-50 text-center">
+		<div className="flex flex-col items-center justify-center h-full text-center">
 			<Cat className="w-20 h-20 text-primary drop-shadow-md" />
 			<h1 className="text-2xl font-semibold text-gray-800">
-				Mew, mew — Welcome to the Admin Zone 🐾
+				Mew, mew — Welcome to the Admin Dashboard 🐾
 			</h1>
 			<p className="text-gray-500 max-w-md">
 				This is your cozy command center. Choose a tab from the sidebar to start
